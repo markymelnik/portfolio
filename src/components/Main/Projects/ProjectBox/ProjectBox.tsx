4c46d154c6b9f53c8bd86ef14f7af43c6d61784d
markymelnik <markemelnik@gmail.com>
2023-10-30 14:33:43 -0400

    create initial grid layout and styling for projects section

---
+++ src/components/Main/Projects/ProjectBox/ProjectBox.tsx
@@ -0,0 +1,53 @@
+import LinkIcon from "../LinkIcon";
+import "./_project-box.scss";
+
+type ProjectBoxProps = {
+  projectId: string;
+  projectYear: number;
+  projectName: string;
+  projectDescription: string;
+  projectDemoUrl?: string;
+  projectSourceUrl?: string;
+};
+
+const ProjectBox = ({
+  projectId,
+  projectYear,
+  projectName,
+  projectDescription,
+  projectDemoUrl,
+  projectSourceUrl,
+}: ProjectBoxProps) => {
+  return (
+    <div className={`project-box ${projectId}`}>
+      <div className="project-box-left">
+      <div className="project-info">
+        <div className="project-year">{projectYear}</div>
+        <div className="project-name">{projectName}</div>
+        <div className="project-description">{projectDescription}</div>
+      </div>
+      <div className="project-links">
+        {projectDemoUrl && (
+          <a href={projectDemoUrl} className="project-demo-link" target="_blank" rel="noopener noreferrer">
+            <span className='project-link-text'>Demo</span>
+            <LinkIcon />
+          </a>
+        )}
+        {projectSourceUrl && (
+          <a href={projectSourceUrl} className="project-code-link" target="_blank" rel="noopener noreferrer">
+            <span className='project-link-text'>Source</span>
+            <LinkIcon />
+          </a>
+        )}
+      </div>
+
+      </div>
+      <div className="project-box-right">
+        <div className="project-image"></div>
+      </div>
+      
+    </div>
+  );
+};
+
+export default ProjectBox;
--- src/components/Main/Projects/Projects.tsx
+++ src/components/Main/Projects/Projects.tsx
@@ -1,12 +1,48 @@
-import { forwardRef } from 'react';
-import './_projects.scss';
+import { forwardRef } from "react";
+import "./_projects.scss";
+import ProjectBox from "./ProjectBox/ProjectBox";
 
 interface ProjectsProps extends React.HTMLProps<HTMLDivElement> {}
 
 const Projects = forwardRef<HTMLDivElement, ProjectsProps>((props, ref) => {
   return (
-    <div ref={ref} className="projects-container" {...props}>Projects</div>
-  )
+    <div ref={ref} className="projects-container" {...props}>
+      <div className="projects-title">Projects</div>
+      <div className="projects-list">
+        <ProjectBox
+          projectId={`project-1`}
+          projectYear={2023}
+          projectName={`markmelnik.com`}
+          projectDescription={`This website!`}
+          projectSourceUrl={`https://google.com`}
+        />
+        <ProjectBox
+          projectId={`project-2`}
+          projectYear={2023}
+          projectName={`FitFinder`}
+          projectDescription={`A web application to find a place to workout.`}
+          projectDemoUrl={`https://fitfinder.app`}
+          projectSourceUrl={`https://google.com`}
+        />
+        <ProjectBox
+          projectId={`project-3`}
+          projectYear={2023}
+          projectName={`Battleship`}
+          projectDescription={`A web-based battleship game.`}
+          projectDemoUrl={`https://google.com`}
+          projectSourceUrl={`https://google.com`}
+        />
+        <ProjectBox
+          projectId={`project-4`}
+          projectYear={2023}
+          projectName={`project-4`}
+          projectDescription={`project description`}
+          projectDemoUrl={`https://google.com`}
+          projectSourceUrl={`https://google.com`}
+        />
+      </div>
+    </div>
+  );
 });
 
 export default Projects;
