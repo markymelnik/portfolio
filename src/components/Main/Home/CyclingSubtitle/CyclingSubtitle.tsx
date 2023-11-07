f312eca4f6609c64b362b2eba054cdf66608193b
markymelnik <markemelnik@gmail.com>
2023-11-07 15:19:27 -0500

    implement vertical text slider for home subtitle

---
+++ src/components/Main/Home/CyclingSubtitle/CyclingSubtitle.tsx
@@ -0,0 +1,14 @@
+import './_cycling-subtitle.scss';
+
+const CyclingSubtitle = () => {
+  return (
+    <h2 className="home-subtitle">
+      <span>Full Stack Developer</span>
+      <span>Lifelong learning 📚</span>
+      <span>Говорит на трёх языках!</span>
+      <span>Very nice, I like 😎</span>
+    </h2>
+  );
+};
+
+export default CyclingSubtitle;
--- src/components/Main/Home/Home.tsx
+++ src/components/Main/Home/Home.tsx
@@ -1,4 +1,5 @@
 import { forwardRef } from 'react';
+import CyclingSubtitle from './CyclingSubtitle/CyclingSubtitle';
 import './_home.scss';
 
 interface HomeProps extends React.HTMLProps<HTMLDivElement> {}
@@ -9,7 +10,7 @@ const Home = forwardRef<HTMLDivElement, HomeProps>((props, ref) => {
     <section ref={ref} className="home-container" {...props}>
       <div className="home-intro">
         <h1 className="home-title">Mark Melnik</h1>
-        <h2 className="home-subtitle">Software Developer</h2>
+        <CyclingSubtitle />
         {/* <h3 className="home-read-more">Read more</h3> */}
       </div>
     </section>
